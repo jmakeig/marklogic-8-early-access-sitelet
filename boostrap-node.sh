0230a79f50e0a2034011e1079d0e75d2a7ac0b33
Justin Makeig <jmpublic@makeig.com>
2014-07-16 10:53:54 -0700

    Got rid of child library in node_modules.

--- boostrap-node.sh
+++ boostrap-node.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-mkdir -p "$1"/node_modules/marklogic
-pushd "$1"/node_modules/marklogic
-git clone git@github.com:marklogic/node-client-api.git
-pushd node-client-api
+mkdir -p "$1"/node_modules/
+pushd "$1"/node_modules/
+git clone git@github.com:marklogic/node-client-api.git marklogic
+pushd marklogic
 npm install
 popd
 popd
-echo 'var client = require("marklogic/node-client-api")
-console.dir(client)' > "$1"/test.js
-node "$1"/test.js
+echo 'var marklogic = require("marklogic")
+console.dir(marklogic)' > "$1"/_test.js
+node "$1"/_test.js
