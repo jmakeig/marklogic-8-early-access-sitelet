ba49087dfb3fc870861dea6379742201e740a9a7
Justin Makeig <jmpublic@makeig.com>
2014-07-21 17:42:14 -0700

    Better relative paths

--- backup.sh
+++ backup.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+pushd `dirname $0` > /dev/null
 # <http://superuser.com/a/399529>
-tar --disable-copyfile --exclude Node.js/node_modules --exclude .git -czf ~/Desktop/8\ EA\ Sitelet.tar.gz .
+tar --disable-copyfile --exclude Node.js/node_modules --exclude .git --exclude .DS_Store -czf ~/Desktop/8\ EA\ Sitelet.tar.gz .
+popd > /dev/null
