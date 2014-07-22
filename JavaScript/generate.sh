2792539d46d22b24f8cf9c77070f1458cd47f246
Justin Makeig <jmpublic@makeig.com>
2014-07-22 11:29:47 -0700

    Parameterized input file and error handling

--- JavaScript/generate.sh
+++ JavaScript/generate.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+if [ -z "$1" ]
+  then
+    echo "Usage: $0 input-filename"
+    exit 1
+fi
 # <http://stackoverflow.com/questions/415677/how-to-replace-placeholders-in-a-text-file>
-eval "echo \"$(< server-side-javascript.html)\"" > ~/tmp/javascript-ea.html
-open ~/tmp/javascript-ea.html
+eval "echo \"$(< $1)\"" > ./javascript-ea.html
+open ./javascript-ea.html
