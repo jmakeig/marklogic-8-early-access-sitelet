1a3db4df10e2f1b69e2b687c681753372fcc3b35
Justin Makeig <jmpublic@makeig.com>
2014-07-22 11:25:09 -0700

    Better file name.

--- JavaScript/generate.sh
+++ JavaScript/generate.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # <http://stackoverflow.com/questions/415677/how-to-replace-placeholders-in-a-text-file>
-eval "echo \"$(< server-side-javascript.html)\"" > ~/tmp/tmpjs.html
-open ~/tmp/tmpjs.html
+eval "echo \"$(< server-side-javascript.html)\"" > ~/tmp/javascript-ea.html
+open ~/tmp/javascript-ea.html
