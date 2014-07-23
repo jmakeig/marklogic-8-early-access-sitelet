12fb08be3f28aa3c96966942e8592bb368e70f63
Justin Makeig <jmpublic@makeig.com>
2014-07-22 17:14:35 -0700

    Pipes to stdout

--- JavaScript/generate.sh
+++ JavaScript/generate.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 if [ -z "$1" ]
   then
-    echo "Usage: $0 input-filename"
+    echo "Inserts file contents into placeholders. Outputs to stdout."
+    echo "Usage: $0 input.html > output.html"
     exit 1
 fi
 # <http://stackoverflow.com/questions/415677/how-to-replace-placeholders-in-a-text-file>
-eval "echo \"$(< $1)\"" > ./javascript-ea.html
-open ./javascript-ea.html
+eval "echo \"$(< $1)\"" >&1
