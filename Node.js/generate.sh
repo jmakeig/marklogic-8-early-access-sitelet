4a604cf94b840ac977da9250b50285d033dffd0d
Justin Makeig <jmpublic@makeig.com>
2014-07-30 17:26:30 -0400

    Some more clean-up

--- Node.js/generate.sh
+++ Node.js/generate.sh
@@ -5,7 +5,14 @@ if [ -z "$1" ]
     echo "Usage: $0 input.html > output.html"
     exit 1
 fi
-# <http://stackoverflow.com/questions/415677/how-to-replace-placeholders-in-a-text-file>
-eval "echo \"$(< $1)\"" >&1
 
+
+#ARGH! How do I do this without a temp file?
+TMP=./~tmp
+sed -E -e 's/\"/\\\"/g' -e 's/\{\{(.+)\}\}/\`cat \"\1\"\`/g' $1 > $TMP 
+# <http://stackoverflow.com/questions/415677/how-to-replace-placeholders-in-a-text-file>
+eval "echo \"$(< $TMP)\"" >&1 # Evaluate the temp file as if it were a script and write it to stdout
+rm $TMP
+
+  
 # ./generate.sh node.html > _node-ea.html; open _node-ea.html 
