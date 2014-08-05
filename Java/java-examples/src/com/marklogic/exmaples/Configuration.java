f846ee9d4043e5eb4f7bb383a0e79205452394fc
Justin Makeig <jmpublic@makeig.com>
2014-08-05 13:49:12 -0700

    Better static initialization

--- Java/java-examples/src/com/marklogic/exmaples/Configuration.java
+++ Java/java-examples/src/com/marklogic/exmaples/Configuration.java
@@ -4,13 +4,14 @@ import com.marklogic.client.DatabaseClient;
 import com.marklogic.client.DatabaseClientFactory;
 
 public class Configuration {
+    private static DatabaseClient client = DatabaseClientFactory.newClient(
+        "jmakeig-centos6-virtualbox.localdomain",
+        8000,
+        "admin",
+        "********",
+        DatabaseClientFactory.Authentication.DIGEST);
 
     public static DatabaseClient exampleClient() {
-        return DatabaseClientFactory.newClient(
-                "jmakeig-centos6-virtualbox.localdomain",
-                8000,
-                "admin",
-                "********",
-                DatabaseClientFactory.Authentication.DIGEST);
+        return client;
     }
 }
