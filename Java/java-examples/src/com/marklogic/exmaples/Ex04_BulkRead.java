df059885d733d4b6f014c189e17e44aa8474c088
Justin Makeig <jmpublic@makeig.com>
2014-08-05 13:49:46 -0700

    Initial commit of bulk read

---
+++ Java/java-examples/src/com/marklogic/exmaples/Ex04_BulkRead.java
@@ -0,0 +1,31 @@
+package com.marklogic.exmaples;
+
+import com.marklogic.client.DatabaseClient;
+import com.marklogic.client.document.DocumentPage;
+import com.marklogic.client.document.DocumentRecord;
+import com.marklogic.client.document.GenericDocumentManager;
+import com.marklogic.client.io.JacksonHandle;
+import com.marklogic.client.query.QueryDefinition;
+import com.marklogic.client.query.StructuredQueryBuilder;
+
+public class Ex04_BulkRead {
+
+    public static void main(String[] args) {
+        DatabaseClient client = Configuration.exampleClient();
+
+        GenericDocumentManager docMgr = client.newDocumentManager();
+
+        // Build a structured query. Here we're doing a pretty broad collection
+        // query on all of the data loaded in Example 1.
+        StructuredQueryBuilder builder = client.newQueryManager().newStructuredQueryBuilder();
+        QueryDefinition query = builder.and(builder.collection("fake data"));
+
+        DocumentPage page = docMgr.search(query, 1L);
+
+        // Iterate through the results, which include the raw documents,
+        // available with a ReadHandle.
+        for (DocumentRecord doc : page) {
+            System.out.println(doc.getContent(new JacksonHandle()));
+        }
+    }
+}
