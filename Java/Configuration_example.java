d2cfea9e91eb899117992e56891d539ce5ebd4c4
Justin Makeig <jmpublic@makeig.com>
2014-08-05 12:06:58 -0700

    More exemplary

--- Java/Configuration_example.java
+++ Java/Configuration_example.java
@@ -8,9 +8,9 @@ public class Configuration {
     public static DatabaseClient exampleClient() {
         return DatabaseClientFactory.newClient(
                 "localhost",
-                8000,
-                "admin",
-                "********",
+                8000,         // A REST instance that backs the Java API is automatically available on port 8000
+                "app-writer", // An application user that has at least the rest-writer role
+                "********",   // Probably not your password
                 DatabaseClientFactory.Authentication.DIGEST);
     }
 }
