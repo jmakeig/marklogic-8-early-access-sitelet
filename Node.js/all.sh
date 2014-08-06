cfb6026de2f9824f4ebea7396c1d3c84dd172f08
Justin Makeig <jmpublic@makeig.com>
2014-08-06 13:15:16 -0700

    Script to run all examples

---
+++ Node.js/all.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+echo "00 teaser"
+node "./00 teaser.js"
+
+echo "01 write"
+node "./01 write.js"
+
+echo "02 query-by-example"
+node "./02 query-by-example.js"
+
+echo "03 queryBuilder"
+node "./03 queryBuilder.js"
+
+echo "04 string query"
+node "./04 string query.js"
+
+echo "05 read"
+node "./05 read.js"
+
+echo "06 patch"
+node "./06 patch.js"
+
+echo "07 remove"
+node "./07 remove.js"
