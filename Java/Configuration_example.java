33bffa7bdc201c81d139ae58af004683b6bc7b12
Justin Makeig <jmpublic@makeig.com>
2014-08-06 10:20:50 -0700

    Wording

--- Java/Configuration_example.java
+++ Java/Configuration_example.java
@@ -6,7 +6,7 @@ import com.marklogic.client.DatabaseClientFactory;
 public class Configuration {
     private static DatabaseClient client = DatabaseClientFactory.newClient(
         "localhost",
-        8000,         // A REST instance that backs the Java API is automatically available on port 8000
+        8000,         // A REST instance that backs the Java API is available by default on port 8000
         "app-writer", // An application user that has at least the rest-writer role
         "********",   // Probably not your password
         DatabaseClientFactory.Authentication.DIGEST);
