a9528d849a2b35accd62025e5239a9ab96a08a8b
Justin Makeig <jmpublic+github@makeig.com>
2014-11-24 10:27:44 -0800

    Copied watcher script from ../Java
    Excludes node_modules and adds output reporting

---
+++ Node.js/watcher.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Usage:
+#  find . -name 'node.html' -o -name "*.js" | grep -v 'node_modules' | entr ./watcher.sh
+# See: <http://entrproject.org>
+
+IN="node.html"
+OUT="_node.html"
+
+./generate.sh "$IN" > "$OUT"
+
+echo "Generated $IN at " `date`
+
+# -g opens in the background
+open -g "$OUT"
